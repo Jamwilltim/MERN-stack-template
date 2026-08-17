166b166573b9e7f6812d81ae6a2e852cd566f8be
James Harvey <122318076+Jamwilltim@users.noreply.github.com>
2026-08-17 14:32:44 +0100

    Update password hashing in user model
    Refactor password hashing logic in user schema.

--- server/models/User.js
+++ server/models/User.js
@@ -29,12 +29,11 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash the password before saving, but only if it was changed
-userSchema.pre("save", async function (next) {
-	if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+	if (!this.isModified("password")) return;
 
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
-	next();
 });
 
 // Instance method to compare a plain-text password against the hashed one
